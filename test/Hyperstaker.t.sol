8e676c59b5011377feda6a9ade94b9795a5be645
Liron Achdut <liron.achdut@gmail.com>
2025-01-03 20:52:49 +0700

    use real hypercerts contracts in tests

--- test/Hyperstaker.t.sol
+++ test/Hyperstaker.t.sol
@@ -3,19 +3,26 @@ pragma solidity ^0.8.28;
 
 import {Test, console} from "forge-std/Test.sol";
 import {Hyperstaker} from "../src/Hyperstaker.sol";
-import {MockHypercertMinter} from "./mocks/MockHypercertMinter.sol";
 import {MockERC20} from "./mocks/MockERC20.sol";
+import {HypercertMinter} from "./hypercerts/HypercertMinter.sol";
 
 contract HyperstakerTest is Test {
     Hyperstaker public hyperstaker;
     MockHypercertMinter public hypercertMinter;
-    uint256 public baseHypercertId = 1<<128;
-    uint256 public fractionHypercertId = (1<<128) + 1;
+    uint256 public baseHypercertId = 1 << 128;
+    uint256 public fractionHypercertId = (1 << 128) + 1;
+    MockERC20 public rewardToken = new MockERC20("Reward", "REW");
 
     function setUp() public {
         hypercertMinter = new MockHypercertMinter();
-        hyperstaker = new Hyperstaker(address(hypercertMinter), baseHypercertId);
+        hyperstaker = new Hyperstaker(address(hypercertMinter), baseHypercertId, address(this));
+        hypercertMinter.setUnits(baseHypercertId, 100);
         hypercertMinter.setUnits(fractionHypercertId, 100);
+    }
+
+    function test_Constructor() public {
+        assertEq(hyperstaker.baseHypercertId(), baseHypercertId);
+        assertEq(hyperstaker.totalUnits(), 100);
     }
 
     function test_Staking() public {
@@ -27,5 +34,10 @@ contract HyperstakerTest is Test {
         hyperstaker.stake(fractionHypercertId);
         hyperstaker.unstake(fractionHypercertId);
         assertEq(hyperstaker.getStake(fractionHypercertId).stakingStartTime, 0);
+    }
+
+    function test_SetReward() public {
+        hyperstaker.setReward(address(0), 100);
+        assertEq(hyperstaker.totalRewards(), 100);
     }
 }
