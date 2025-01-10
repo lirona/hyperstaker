174821c96ef46d147452a4d4a2cf2b1d48d080c9
Prajjawalk <prajjawalkhandelwal@gmail.com>
2025-01-10 22:55:09 +0530

    Add: hyperfactory

---
+++ src/HyperfundFactory.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import "./Hyperfund.sol"; // Import Hyperfund contract
+import "./Hyperstaker.sol"; // Import Hyperstaker contract
+
+contract HyperfundFactory {
+    Hyperfund[] public hyperfunds; // Array to store deployed Hyperfund contracts
+    Hyperstaker[] public hyperstakers; // Array to store deployed Hyperstaker contracts
+
+    // Mapping to associate (manager address, hypercert ID) with Hyperfund and Hyperstaker addresses
+    mapping(address => mapping(uint256 => Hyperfund)) public hyperfundsByManager;
+    mapping(address => mapping(uint256 => Hyperstaker)) public hyperstakersByManager;
+
+    // Event to emit when a new Hyperfund is created
+    event HyperfundCreated(address indexed hyperfundAddress, address indexed manager, uint256 hypercertId);
+
+    // Event to emit when a new Hyperstaker is created
+    event HyperstakerCreated(address indexed hyperstakerAddress, address indexed manager, uint256 hypercertId);
+
+    // Function to create a new Hyperfund
+    function createHyperfund(address hypercertMinter, uint256 hypercertId, address manager) external {
+        require(hypercertMinter != address(0), "Invalid hypercert minter");
+        require(manager != address(0), "Invalid manager");
+        
+        Hyperfund newHyperfund = new Hyperfund(hypercertMinter, hypercertId, manager);
+        require(address(newHyperfund) != address(0), "Hyperfund deployment failed");
+        
+        hyperfunds.push(newHyperfund);
+        hyperfundsByManager[manager][hypercertId] = newHyperfund;
+        emit HyperfundCreated(address(newHyperfund), manager, hypercertId);
+    }
+
+    // Function to create a new Hyperstaker
+    function createHyperstaker(address hypercertMinter, uint256 hypercertId, address manager) external {
+        require(hypercertMinter != address(0), "Invalid hypercert minter");
+        require(manager != address(0), "Invalid manager");
+        
+        Hyperstaker newHyperstaker = new Hyperstaker(hypercertMinter, hypercertId, manager);
+        require(address(newHyperstaker) != address(0), "Hyperstaker deployment failed");
+        
+        hyperstakers.push(newHyperstaker);
+        hyperstakersByManager[manager][hypercertId] = newHyperstaker;
+        emit HyperstakerCreated(address(newHyperstaker), manager, hypercertId);
+    }
+
+    // Function to get all deployed Hyperfunds
+    function getHyperfunds() external view returns (Hyperfund[] memory) {
+        return hyperfunds; // Return the array of Hyperfunds
+    }
+
+    // Function to get all deployed Hyperstakers
+    function getHyperstakers() external view returns (Hyperstaker[] memory) {
+        return hyperstakers; // Return the array of Hyperstakers
+    }
+}
+
---
+++ test/HyperfundFactory.t.sol
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import {Test} from "forge-std/Test.sol";
+import "../src/HyperfundFactory.sol";
+import "../src/Hyperfund.sol";
+import "../src/Hyperstaker.sol";
+import {HypercertMinter} from "./hypercerts/HypercertMinter.sol";
+import {IHypercertToken as HT} from "./hypercerts/IHypercertToken.sol";
+
+contract HyperfundFactoryTest is Test {
+    HyperfundFactory hyperfundFactory;
+    Hyperfund hyperfund;
+    Hyperstaker hyperstaker;
+
+    HypercertMinter public hypercertMinter;
+    uint256 hypercertId;
+    address manager;
+    uint256 public totalUnits = 100000000;
+
+    function setUp() public {
+        hyperfundFactory = new HyperfundFactory();
+        manager = address(this);
+        hypercertMinter = new HypercertMinter();
+        hypercertId = hypercertMinter.mintClaim(
+            address(this),
+            totalUnits,
+            "uri",
+            HT.TransferRestrictions.AllowAll
+        );
+    }
+
+    function testCreateHyperfund() public {
+        hyperfundFactory.createHyperfund(
+            address(hypercertMinter),
+            hypercertId,
+            manager
+        );
+
+        Hyperfund createdHyperfund = hyperfundFactory.hyperfundsByManager(
+            manager,
+            hypercertId
+        );
+        assertTrue(
+            address(createdHyperfund) != address(0),
+            "Hyperfund should be created and mapped correctly"
+        );
+    }
+
+    function testCreateHyperstaker() public {
+        hyperfundFactory.createHyperstaker(
+            address(hypercertMinter),
+            hypercertId,
+            manager
+        );
+
+        Hyperstaker createdHyperstaker = hyperfundFactory.hyperstakersByManager(
+            manager,
+            hypercertId
+        );
+        assertTrue(
+            address(createdHyperstaker) != address(0),
+            "Hyperstaker should be created and mapped correctly"
+        );
+    }
+
+    function testGetHyperfunds() public {
+        hyperfundFactory.createHyperfund(
+            address(hypercertMinter),
+            hypercertId,
+            manager
+        );
+        Hyperfund[] memory hyperfunds = hyperfundFactory.getHyperfunds();
+
+        assertEq(
+            hyperfunds.length,
+            1,
+            "There should be one deployed Hyperfund"
+        );
+    }
+
+    function testGetHyperstakers() public {
+        hyperfundFactory.createHyperstaker(
+            address(hypercertMinter),
+            hypercertId,
+            manager
+        );
+        Hyperstaker[] memory hyperstakers = hyperfundFactory.getHyperstakers();
+
+        assertEq(
+            hyperstakers.length,
+            1,
+            "There should be one deployed Hyperstaker"
+        );
+    }
+
+    function onERC1155Received(
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes calldata
+    ) external pure returns (bytes4) {
+        return
+            bytes4(
+                keccak256(
+                    "onERC1155Received(address,address,uint256,uint256,bytes)"
+                )
+            );
+    }
+}
