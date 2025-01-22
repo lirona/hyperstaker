04ff9aed1d9c8d36d42e0de0532ca6ce8f3ca74d
Prajjawalk <prajjawalkhandelwal@gmail.com>
2025-01-23 01:06:09 +0530

    Add: upgradable + redeploy check + custom errors

--- src/HyperfundFactory.sol
+++ src/HyperfundFactory.sol
@@ -3,11 +3,18 @@ pragma solidity ^0.8.28;
 
 import "./Hyperfund.sol"; // Import Hyperfund contract
 import "./Hyperstaker.sol"; // Import Hyperstaker contract
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract HyperfundFactory {
-    // Mapping to associate (manager address, hypercert ID) with Hyperfund and Hyperstaker addresses
+contract HyperfundFactory is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+    // Mapping to associate (hypercert ID) with Hyperfund and Hyperstaker addresses
     mapping(uint256 => address) public hyperfunds;
     mapping(uint256 => address) public hyperstakers;
+
+    error InvalidHypercertMinter();
+    error InvalidManager();
+    error DeploymentFailed();
 
     // Event to emit when a new Hyperfund is created
     event HyperfundCreated(address indexed hyperfundAddress, address indexed manager, uint256 hypercertId);
@@ -15,16 +22,31 @@ contract HyperfundFactory {
     // Event to emit when a new Hyperstaker is created
     event HyperstakerCreated(address indexed hyperstakerAddress, address indexed manager, uint256 hypercertId);
 
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
     // Function to create a new Hyperfund
     function createHyperfund(address hypercertMinter, uint256 hypercertId, address manager)
         external
         returns (address)
     {
-        require(hypercertMinter != address(0), "Invalid hypercert minter");
-        require(manager != address(0), "Invalid manager");
+        require(hypercertMinter != address(0), InvalidHypercertMinter());
+        require(manager != address(0), InvalidManager());
+        if (hyperfunds[hypercertId] != address(0)) {
+            return hyperfunds[hypercertId];
+        }
 
         address newHyperfund = address(new Hyperfund(hypercertMinter, hypercertId, manager));
-        require(newHyperfund != address(0), "Hyperfund deployment failed");
+        require(newHyperfund != address(0), DeploymentFailed());
 
         hyperfunds[hypercertId] = newHyperfund;
         emit HyperfundCreated(newHyperfund, manager, hypercertId);
@@ -36,11 +58,14 @@ contract HyperfundFactory {
         external
         returns (address)
     {
-        require(hypercertMinter != address(0), "Invalid hypercert minter");
-        require(manager != address(0), "Invalid manager");
+        require(hypercertMinter != address(0), InvalidHypercertMinter());
+        require(manager != address(0), InvalidManager());
+        if (hyperstakers[hypercertId] != address(0)) {
+            return hyperstakers[hypercertId];
+        }
 
         address newHyperstaker = address(new Hyperstaker(hypercertMinter, hypercertId, manager));
-        require(newHyperstaker != address(0), "Hyperstaker deployment failed");
+        require(newHyperstaker != address(0), DeploymentFailed());
 
         hyperstakers[hypercertId] = newHyperstaker;
         emit HyperstakerCreated(newHyperstaker, manager, hypercertId);
--- test/HyperfundFactory.t.sol
+++ test/HyperfundFactory.t.sol
@@ -7,66 +7,129 @@ import "../src/Hyperfund.sol";
 import "../src/Hyperstaker.sol";
 import {HypercertMinter} from "./hypercerts/HypercertMinter.sol";
 import {IHypercertToken as HT} from "./hypercerts/IHypercertToken.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {MockHyperfundFactoryV2} from "./mocks/MockHyperfundFactoryV2.sol";
 
 contract HyperfundFactoryTest is Test {
+    HyperfundFactory implementation;
     HyperfundFactory hyperfundFactory;
     Hyperfund hyperfund;
     Hyperstaker hyperstaker;
-    
 
     address public hypercertMinter;
     uint256 hypercertId;
     address manager;
     uint256 public totalUnits = 100000000;
 
+    event Upgraded(address indexed implementation);
+
     function setUp() public {
-        hyperfundFactory = new HyperfundFactory();
+        // Deploy implementation
+        implementation = new HyperfundFactory();
+
+        // Deploy proxy
+        bytes memory initData = abi.encodeWithSelector(HyperfundFactory.initialize.selector);
+        ERC1967Proxy proxy = new ERC1967Proxy(address(implementation), initData);
+
+        // Get factory instance
+        hyperfundFactory = HyperfundFactory(address(proxy));
+
         manager = address(this);
         hypercertMinter = address(new HypercertMinter());
-        hypercertId = HypercertMinter(hypercertMinter).mintClaim(address(this), totalUnits, "uri", HT.TransferRestrictions.AllowAll);
+        hypercertId = HypercertMinter(hypercertMinter).mintClaim(
+            address(this), totalUnits, "uri", HT.TransferRestrictions.AllowAll
+        );
     }
 
-    function testCreateHyperfund() public {
+    function test_InitialOwnership() public view {
+        assertEq(hyperfundFactory.owner(), address(this));
+    }
+
+    function test_CannotInitializeTwice() public {
+        vm.expectRevert(Initializable.InvalidInitialization.selector);
+        hyperfundFactory.initialize();
+    }
+
+    function test_CannotUpgradeFromNonOwner() public {
+        MockHyperfundFactoryV2 newImplementation = new MockHyperfundFactoryV2();
+
+        address user = makeAddr("user");
+        vm.prank(user);
+
+        vm.expectRevert(abi.encodeWithSelector(OwnableUpgradeable.OwnableUnauthorizedAccount.selector, user));
+        hyperfundFactory.upgradeToAndCall(address(newImplementation), "");
+    }
+
+    function test_SuccessfulUpgrade() public {
+        MockHyperfundFactoryV2 newImplementation = new MockHyperfundFactoryV2();
+
+        vm.expectEmit(true, false, false, false);
+        emit Upgraded(address(newImplementation));
+
+        hyperfundFactory.upgradeToAndCall(address(newImplementation), "");
+
+        // Cast to V2 to test new functionality
+        MockHyperfundFactoryV2 upgradedFactory = MockHyperfundFactoryV2(address(hyperfundFactory));
+        assertEq(upgradedFactory.version(), 2);
+    }
+
+    function test_CreateHyperfund() public {
         hyperfundFactory.createHyperfund(hypercertMinter, hypercertId, manager);
 
         address createdHyperfund = hyperfundFactory.hyperfunds(hypercertId);
         assertTrue(createdHyperfund != address(0), "Hyperfund should be created and mapped correctly");
     }
 
-    function testCreateHyperstaker() public {
+    function test_CreateHyperstaker() public {
         hyperfundFactory.createHyperstaker(hypercertMinter, hypercertId, manager);
 
         address createdHyperstaker = hyperfundFactory.hyperstakers(hypercertId);
-        assertTrue(address(createdHyperstaker) != address(0), "Hyperstaker should be created and mapped correctly");
+        assertTrue(createdHyperstaker != address(0), "Hyperstaker should be created and mapped correctly");
     }
 
-    function testCreateHyperfundZeroMinter() public {
-        vm.expectRevert("Invalid hypercert minter");
+    function test_ReturnDeployedHyperfundWhen_RedeployingHyperfundWithSameHypercertId() public {
+        address createdHyperfund = hyperfundFactory.createHyperfund(hypercertMinter, hypercertId, manager);
+
+        address returnedHyperfundAddress = hyperfundFactory.createHyperfund(hypercertMinter, hypercertId, manager);
+        assertTrue(createdHyperfund == returnedHyperfundAddress, "Should not redeploy hyperfund if one already exists");
+    }
+
+    function test_ReturnDeployedHyperStakerWhen_RedeployingHyperStakerWithSameHypercertId() public {
+        address createdHyperstaker = hyperfundFactory.createHyperstaker(hypercertMinter, hypercertId, manager);
+
+        address returnedHyperstakerAddress = hyperfundFactory.createHyperstaker(hypercertMinter, hypercertId, manager);
+        assertTrue(
+            createdHyperstaker == returnedHyperstakerAddress, "Should not redeploy hyperstaker if one already exists"
+        );
+    }
+
+    function test_RevertWhen_CreateHyperfundZeroMinter() public {
+        vm.expectRevert(HyperfundFactory.InvalidHypercertMinter.selector);
         hyperfundFactory.createHyperfund(address(0), hypercertId, manager);
     }
 
-    function testCreateHyperfundZeroManager() public {
-        vm.expectRevert("Invalid manager");
+    function test_RevertWhen_CreateHyperfundZeroManager() public {
+        vm.expectRevert(HyperfundFactory.InvalidManager.selector);
         hyperfundFactory.createHyperfund(hypercertMinter, hypercertId, address(0));
     }
 
-    function testCreateHyperstakerZeroMinter() public {
-        vm.expectRevert("Invalid hypercert minter");
+    function test_RevertWhen_CreateHyperstakerZeroMinter() public {
+        vm.expectRevert(HyperfundFactory.InvalidHypercertMinter.selector);
         hyperfundFactory.createHyperstaker(address(0), hypercertId, manager);
     }
 
-    function testCreateHyperstakerZeroManager() public {
-        vm.expectRevert("Invalid manager");
+    function test_RevertWhen_CreateHyperstakerZeroManager() public {
+        vm.expectRevert(HyperfundFactory.InvalidManager.selector);
         hyperfundFactory.createHyperstaker(hypercertMinter, hypercertId, address(0));
     }
 
-    function testFailedHyperfundDeployment() public {
-        vm.expectRevert("Deployment failed");
+    function test_RevertWhen_FailedHyperfundDeployment() public {
+        vm.expectRevert();
         hyperfundFactory.createHyperfund(address(0x0000000000000000000000000001), hypercertId, manager);
     }
 
-    function testFailedHyperstakerDeployment() public {
-        vm.expectRevert("Deployment failed");
+    function test_RevertWhen_FailedHyperstakerDeployment() public {
+        vm.expectRevert();
         hyperfundFactory.createHyperstaker(address(0x0000000000000000000000000001), hypercertId, manager);
     }
 
